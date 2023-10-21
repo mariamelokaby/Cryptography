4fb0e4f83b864dfd748b1bebd03865646a1d128e
Mariam-mimi7 <134174281+Mariam-mimi7@users.noreply.github.com>
2023-10-21 20:43:06 +0300

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,61 +1,57 @@
+
 use sha2::{Digest, Sha256};
-pub trait SumCommitment {
+use generic_array::typenum::U32;
+use generic_array::GenericArray;
+
+use std::fmt::Debug;
+
+// Define the SumCommitment trait
+pub trait SumCommitment: Debug + Clone {
     fn amount(&self) -> u64;
-    fn digest(&self) -> [u8; 32];
+    fn digest(&self) -> GenericArray<u8, U32>;
+    fn combine_commitments(left: &Self, right: &Self) -> Self;
+    fn new(balance: u64) -> Self;
 }
 
-pub trait ExclusiveAllotmentProof<C: SumCommitment> {
+// Define the ExclusiveAllotmentProof trait
+pub trait ExclusiveAllotmentProof<C: SumCommitment>: Debug {
+    fn new(position: usize, sibling: Option<C>) -> Self;
     fn position(&self) -> usize;
-    fn sibling(&self) -> Option<&C>;
+    fn sibling(&self) -> Option<C>;
     fn verify(&self, root_commitment: &C) -> bool;
-    fn generate_proof(position: usize, sibling: Option<&C>) -> Self;
+    fn reconstruct_commitment(&self, root_commitment: &C) -> C;
+    fn reconstruct_commitment_recursive(&self, node_index: usize, root_commitment: &C) -> C;
 }
 
-pub trait MerkleTree<C: SumCommitment> {
-    type P: ExclusiveAllotmentProof<C>;
-
-    fn new(values: Vec<u64>) -> Self;
-    fn commit(&self) -> C;
-    fn prove(&self, position: usize) -> Self::P;
+// Define the MerkleProof struct
+#[derive(Debug)]
+pub struct MerkleProof<C: SumCommitment> {
+    sibling: Option<C>,
+    sibling_position: usize,
 }
 
-fn hash_bytes(slice: &[u8; 32]) -> [u8; 32] {
-    let mut hasher = Sha256::new();
-    hasher.update(slice);
-    hasher.finalize().into()
-}
-#[derive(Clone, PartialEq)]
-pub struct MimiSumCommitmentWrapper {
-    inner: MimiSumCommitment,
-}
+impl<C: SumCommitment> MerkleProof<C> {
+    pub fn new(sibling: Option<C>, sibling_position: usize) -> Self {
+        MerkleProof {
+            sibling,
+            sibling_position,
+        }
+    }
 
-impl MimiSumCommitmentWrapper {
-    pub fn new(amount: u64, digest: [u8; 32]) -> Self {
-        let inner = MimiSumCommitment { amount, digest };
-        MimiSumCommitmentWrapper { inner }
+    pub fn sibling(&self) -> Option<C> {
+        self.sibling.clone()
+    }
+
+    pub fn sibling_position(&self) -> usize {
+        self.sibling_position
     }
 }
 
-impl SumCommitment for MimiSumCommitmentWrapper {
-    fn amount(&self) -> u64 {
-        self.inner.amount
-    }
-
-    fn digest(&self) -> [u8; 32] {
-        self.inner.digest
-    }
-}
-impl MimiSumCommitment {
-    pub fn new(amount: u64, digest: [u8; 32]) -> Self {
-        MimiSumCommitment { amount, digest }
-    }
-}
-
-
-#[derive(Clone, PartialEq)]
+// Define MimiSumCommitment struct
+#[derive(Debug, Clone)]
 pub struct MimiSumCommitment {
-    pub amount: u64,
-    pub digest: [u8; 32],
+    amount: u64,
+    digest: GenericArray<u8, U32>,
 }
 
 impl SumCommitment for MimiSumCommitment {
@@ -63,128 +59,107 @@ impl SumCommitment for MimiSumCommitment {
         self.amount
     }
 
-    fn digest(&self) -> [u8; 32] {
-        self.digest
-    }
-}
-
-pub struct MimiExclusiveAllotmentProof<C: SumCommitment> {
-    pub position: usize,
-    pub sibling: Option<C>,
-}
-
-impl<C> ExclusiveAllotmentProof<C> for MimiExclusiveAllotmentProof<C>
-where
-    C: SumCommitment + Clone + PartialEq,
-{
-    fn position(&self) -> usize {
-        self.position
+    fn digest(&self) -> GenericArray<u8, U32> {
+        self.digest.clone()
     }
 
-    fn sibling(&self) -> Option<&C> {
-        self.sibling.as_ref()
-    }
+    fn combine_commitments(left: &Self, right: &Self) -> Self {
+        let combined_amount = left.amount() + right.amount();
+        let combined_digest = hash_bytes(&[left.digest(), right.digest()].concat());
 
-    fn verify(&self, root_commitment: &C) -> bool {
-        if let Some(sibling) = &self.sibling {
-            let computed_commitment = compute_merkle_commitment(self.position(), sibling, root_commitment);
-            computed_commitment == *root_commitment
-        } else {
-            false
+        MimiSumCommitment {
+            amount: combined_amount,
+            digest: combined_digest,
         }
     }
 
-    fn generate_proof(position: usize, sibling: Option<&C>) -> Self {
-        MimiExclusiveAllotmentProof {
-            position,
-            sibling: sibling.cloned(),
+    fn new(balance: u64) -> Self {
+        MimiSumCommitment {
+            amount: balance,
+            digest: hash_bytes(&balance.to_le_bytes()),
         }
     }
 }
 
-pub struct MimiMerkleTree<C: SumCommitment, P: ExclusiveAllotmentProof<C>> {
+fn hash_bytes(slice: &[u8]) -> [u8; 32] {
+    let mut hasher = Sha256::new();
+    hasher.update(slice);
+    let result = hasher.finalize();
+    let mut output = [0u8; 32];
+    output.copy_from_slice(result.as_slice());
+    output
+}
+
+// Define MimiMerkleTree struct
+pub struct MimkMerkleTree<C: SumCommitment, P: ExclusiveAllotmentProof<C>> {
     leaf_nodes: Vec<C>,
-    proof: P,
 }
 
-impl<C, P> MimiMerkleTree<C, P>
+impl<C, P> MimkMerkleTree<C, P>
 where
     C: SumCommitment + Clone + PartialEq,
-    P: ExclusiveAllotmentProof<C>, Vec<C>: FromIterator<MimiSumCommitment>
+    P: ExclusiveAllotmentProof<C>,
 {
     pub fn new(values: Vec<u64>) -> Self {
-        let leaf_nodes: Vec<C> = values
-            .iter()
-            .map(|&amount| MimiSumCommitment {
-                amount,
-                digest: [0; 32],
-            })
-            .collect();
-
-        // Initialize proof as needed based on your requirements
-        let proof = P::generate_proof(0, None);
-
-        MimiMerkleTree {
-            leaf_nodes,
-            proof,
-        }
+        let leaf_nodes: Vec<C> = values.into_iter().map(|balance| C::new(balance)).collect();
+        Self { leaf_nodes }
     }
 
     pub fn commit(&self) -> C {
-        self.compute_root_commitment(0, 0, self.leaf_nodes.len())
+        self.build_merkle_tree(0, &self.leaf_nodes)
     }
 
     pub fn prove(&self, position: usize) -> P {
-        self.generate_proof(position, None)
+        let proof = self.construct_inclusion_proof(position, 0, &self.leaf_nodes);
+        P::new(position, proof.sibling)
     }
 
-    fn compute_root_commitment(&self, node_index: usize, start: usize, end: usize) -> C {
-        if start == end {
-            self.leaf_nodes[node_index].clone()
+    fn build_merkle_tree(&self, node_index: usize, nodes: &[C]) -> C {
+        if nodes.len() == 1 {
+            return nodes[0].clone();
+        }
+        let middle = nodes.len() / 2;
+        let left = &nodes[0..middle];
+        let right = &nodes[middle..];
+
+        let left_commitment = self.build_merkle_tree(node_index * 2 + 1, left);
+        let right_commitment = self.build_merkle_tree(node_index * 2 + 2, right);
+
+        C::combine_commitments(&left_commitment, &right_commitment)
+    }
+
+    fn construct_inclusion_proof(
+        &self,
+        position: usize,
+        node_index: usize,
+        nodes: &[C],
+    ) -> MerkleProof<C> {
+        if nodes.len() == 1 {
+            return MerkleProof::new(None, node_index);
+        }
+
+        let middle = nodes.len() / 2;
+        if position < middle {
+            // The position is in the left subtree
+            let right_commitment = self.build_merkle_tree(node_index * 2 + 2, &nodes[middle..]);
+            let right_sibling_position = node_index * 2 + 1;
+            MerkleProof::new(Some(right_commitment), right_sibling_position)
         } else {
-            let midpoint = (start + end) / 2;
-            let left_commitment = self.compute_root_commitment(node_index * 2 + 1, start, midpoint);
-            let right_commitment = self.compute_root_commitment(node_index * 2 + 2, midpoint, end);
-            self.combine_commitments(&left_commitment, &right_commitment)
+            // The position is in the right subtree
+            let left_commitment = self.build_merkle_tree(node_index * 2 + 1, &nodes[0..middle]);
+            let left_sibling_position = node_index * 2 + 2;
+            MerkleProof::new(Some(left_commitment), left_sibling_position)
         }
     }
-
-    fn combine_commitments(&self, left: &C, right: &C) -> C {
-        let mut hasher = Sha256::new();
-        hasher.update(&left.digest());
-        hasher.update(&right.digest());
-        let result = hasher.finalize();
-    
-        C::new(left.amount() + right.amount(), result.into())
-    }
-
-    fn generate_proof(&self, position: usize, sibling: Option<C>) -> P {
-        P::generate_proof(position, sibling.as_ref())
-    }
 }
-
-fn compute_merkle_commitment<C: SumCommitment>(position: usize, sibling: &C, root_commitment: &C) -> MimiSumCommitmentWrapper {
-    let sibling_digest = hash_bytes(&sibling.digest());
-    let root_digest = hash_bytes(&root_commitment.digest());
-
-    let mut hasher = Sha256::new();
-    hasher.update(&sibling_digest);
-    hasher.update(&root_digest);
-    let result = hasher.finalize();
-
-    MimiSumCommitmentWrapper {
-        inner: MimiSumCommitment {
-            amount: root_commitment.amount(),
-            digest: result.into(),
-        },
-    }
-}
-
 
 fn main() {
-    let wrapper = MimiSumCommitmentWrapper::new(42, [0; 32]);
-    let merkle_tree = MimiMerkleTree::new(vec![100, 200, 300, 400]);
-    let commitment: MimiSumCommitment = merkle_tree.commit();
-    let proof: MimiExclusiveAllotmentProof<MimiSumCommitment> = merkle_tree.prove(2);
-    println!("Is the proof valid? {}", proof.verify(&commitment));
+    let values = vec![100, 200, 300, 400, 500];
+    let merkle_tree = MimkMerkleTree::<MimiSumCommitment, MerkleProof<MimiSumCommitment>>::new(values);
+    let commitment = merkle_tree.commit();
+    let proof = merkle_tree.prove(2);
+
+    println!("Root Commitment: {:?}", commitment);
+    println!("Proof: {:?}", proof);
 }
+
